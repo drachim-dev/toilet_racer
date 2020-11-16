fc461c2b1d29010a184d5a546f38ba8819487888
Tim Wiechmann <tim.wiechmann@web.de>
2020-11-16 18:27:15 +0100

    Add basic flame game.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
+import 'package:flame/components/component.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flame/gestures.dart';
+import 'package:flame/position.dart';
+import 'package:flame/sprite.dart';
 import 'package:flutter/material.dart';
-import 'package:toilet_racer/view/home.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final game = MyGame();
+  runApp(game.widget);
 }
 
-class MyApp extends StatelessWidget {
-  static const title = 'Toilet Racer';
+// Extend Box2DGame class to use a physics engine
+class MyGame extends BaseGame with TapDetector {
+  MyPlayer _player;
+
+  MyGame() {
+    _init();
+    _start();
+  }
+
+  void _init() {
+    _player = MyPlayer();
+
+    add(_player);
+  }
+
+  void _start() {
+    // Flame.audio.loop('background.mp3', volume: 0.4);
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: title,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeView(title: title),
-    );
+  void onTap() {
+    final double sizeDiff = 5;
+    final double positionDiff = 10;
+
+    // position upwards
+    _player.x -= sizeDiff / 2;
+    _player.y -= positionDiff;
+
+    // increase size
+    _player.width += sizeDiff;
+    _player.height += sizeDiff;
+
+    _player.toPosition();
+    _player.toSize();
+  }
+
+}
+
+class MyPlayer extends SpriteComponent {
+  MyPlayer() : super.fromSprite(48, 48, Sprite('tomato.png'));
+
+  @override
+  void resize(Size size) {
+    // we don't need to set the x and y in the constructor, we can set then here
+    this.x = (size.width - this.width) / 2;
+    this.y = (size.height - this.height) / 2;
   }
 }
